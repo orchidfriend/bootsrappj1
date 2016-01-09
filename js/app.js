50d463a76dff7a0907c21ca50a548c52bc02b568
orchidfriend <orchidfriend@gmail.com>
2016-01-09 13:09:19 +0000

    Hook firebase and complete contact function.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-var app = angular.module("contactApp",[]);
+var app = angular.module("contactApp",['firebase']);
 
-app.controller("mainController",function($scope){
+app.constant('fbURL',"https://bootstraptest-rollen.firebaseio.com/");
+
+app.factory("contactData",function($firebaseArray,fbURL){
+    return $firebaseArray(new Firebase(fbURL));
+});
+
+app.controller("mainController",function($scope,$firebaseArray,contactData){
     $scope.text = "1234";
+    $scope.contactDB = contactData;
+    $scope.sendMessage = function(contactInfo){
+        if (!contactInfo || !contactInfo.email)
+            return;
+        contactData.$add({
+            name: contactInfo.name,
+            email: contactInfo.email,
+            subject: contactInfo.subject,
+            message: contactInfo.message
+        });
+/*        
+        contactInfo.name = "";
+        contactInfo.email = "";
+        contactInfo.subject = "";
+        contactInfo.message = "";
+*/
+    };
 });
 
